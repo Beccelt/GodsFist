953a1b1917227661736ce4b079a74120aaf56743
Hiren Bhavsar <bhavsar.hsb@gmail.com>
2015-02-14 13:45:12 -0500

    Added stuff from MMXVRoboter

---
+++ src/godsfist/robot/Sensors.java
@@ -0,0 +1,61 @@
+package godsfist.robot;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Gyro;
+
+/**
+ *
+ * @author Jared 'jarebear' Gentner <jargen3d@yahoo.com>
+ * @author Hiren 'BhavTsar' Bhavsar <bhavsar.hsb@gmail.com>
+ */
+public class Sensors {
+
+	static {
+		gyro = new Gyro(0);
+		trayExtendedSwitch = new DigitalInput(0);
+		trayRetractedSwitch = new DigitalInput(0);
+		elevatorLoweredSwitch = new DigitalInput(0);
+		toteInClawSwitch = new DigitalInput(0);
+		clawOpenedSwitch = new DigitalInput(0);
+		clawClosedSwitch = new DigitalInput(0);
+	}
+
+	/**
+	 * Gyro to determine angle of robot relative to field. Possible uses could
+	 * be for fieldcentric drive or to maintain a specific angle.
+	 */
+	public static final Gyro gyro;
+
+	/**
+	 * Switch to determine if the tray to receive totes from the feeding station
+	 * is fully extended.
+	 */
+	public static final DigitalInput trayExtendedSwitch;
+
+	/**
+	 * Switch to determine if the tray to receive totes from the feeding station
+	 * is fully extended.
+	 */
+	public static final DigitalInput trayRetractedSwitch;
+
+	/**
+	 * Switch to determine if the elevator is lowered fully, used for
+	 * calibrating the encoder and for changing states.
+	 */
+	public static final DigitalInput elevatorLoweredSwitch;
+
+	/**
+	 * Switch to determine if an object is in the claw.
+	 */
+	public static final DigitalInput toteInClawSwitch;
+
+	/**
+	 * Switch to determine if the claw is fully open.
+	 */
+	public static final DigitalInput clawOpenedSwitch;
+
+	/**
+	 * Switch to determine if the claw is fully closed.
+	 */
+	public static final DigitalInput clawClosedSwitch;
+}
---
+++ src/godsfist/robot/SystemsCheck.java
@@ -0,0 +1,9 @@
+package godsfist.robot;
+
+public class SystemsCheck implements Runnable {
+
+	@Override
+	public void run() {
+		
+	}
+}
